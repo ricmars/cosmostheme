43f17d1bb7fbed3f1fbc1e7ee18299af77caa641
Richard Marsot <29442140+ricmars@users.noreply.github.com>
2020-10-03 12:44:51 -0400

    Update cosmosthemeswitcher.js

--- cosmosthemeswitcher.js
+++ cosmosthemeswitcher.js
@@ -8,13 +8,14 @@ var colorSwatches = { 'light' : ['#FFF', '#FFF', '#000'],
             'deep cerise': ['#D3197C', '#000', '#FFF'],
           };
 var cssInlineVars;
+function CloseCosmosDevTools(event) {
+  event.preventDefault();
+  document.body.removeChild(cosmosDevToolbar);
+}
+
 function switchColor(event) {
   var themeColor = event.target.textContent;
-  if(event.target.className === 'icon-close' || event.target.parentNode.className === 'icon-close' ) {
-    document.body.removeChild(cosmosDevToolbar);
-    return;
-  }
-   if(typeof (colorSwatches[themeColor]) === 'undefined') {
+  if(typeof (colorSwatches[themeColor]) === 'undefined') {
     return;
   }
   var generalBgColor = colorSwatches[themeColor][0];
@@ -38,7 +39,7 @@ function switchColor(event) {
   document.getElementsByTagName("head")[0].appendChild(cssInlineVars); 
 }
 function RunCosmosDevTools() {
-  var colorPicker = `<div onclick='switchColor(event);'><div><h4>Theme switcher</h4><button aria-label='close' class='icon-close'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="20px" height="20px" fill="currentColor">
+  var colorPicker = `<div onclick='switchColor(event);'><div><h4>Theme switcher</h4><button onclick='CloseCosmosDevTools(event);' aria-label='Close the theme switcher' class='icon-close'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="20px" height="20px" fill="currentColor">
 <path d="M20.3375,3 C20.3375,3 21.7742187,4.43671875 21.7742187,4.43671875 C21.7742187,4.43671875 4.43671875,21.4867188 4.43671875,21.4867188 C4.43671875,21.4867188 3,20.19375 3,20.19375 C3,20.19375 20.3375,3 20.3375,3 Z M21.7265625,20.0976563 C21.7265625,20.0976563 20.3859375,21.534375 20.3859375,21.534375 C20.3859375,21.534375 3.00078125,4.3890625 3.00078125,4.3890625 C3.00078125,4.3890625 4.38984375,3 4.38984375,3 C4.38984375,3 21.7273438,20.0976563 21.7273438,20.0976563 L21.7265625,20.0976563 Z"/>
 </svg></button></div><div>`;
   for(var i in colorSwatches) {
