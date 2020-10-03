f88434801e0b799442443edc2519b6aa855355ce
Richard Marsot <29442140+ricmars@users.noreply.github.com>
2020-10-03 14:56:28 -0400

    Update cosmosthemeswitcher.js

--- cosmosthemeswitcher.js
+++ cosmosthemeswitcher.js
@@ -15,6 +15,11 @@ function CloseCosmosDevTools(event) {
 
 function switchColor(event) {
   var themeColor = event.target.textContent;
+  if(themeColor === 'reset') {
+    document.body.removeChild(cosmosCSS);
+    document.body.removeChild(cosmosDevToolbar);
+    return;
+  }
   if(typeof (colorSwatches[themeColor]) === 'undefined') {
     return;
   }
@@ -41,7 +46,7 @@ function switchColor(event) {
 function RunCosmosDevTools() {
   var colorPicker = `<div onclick='switchColor(event);'><div><h4>Theme switcher</h4><button onclick='CloseCosmosDevTools(event);' aria-label='Close the theme switcher' class='icon-close'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="20px" height="20px" fill="currentColor">
 <path d="M20.3375,3 C20.3375,3 21.7742187,4.43671875 21.7742187,4.43671875 C21.7742187,4.43671875 4.43671875,21.4867188 4.43671875,21.4867188 C4.43671875,21.4867188 3,20.19375 3,20.19375 C3,20.19375 20.3375,3 20.3375,3 Z M21.7265625,20.0976563 C21.7265625,20.0976563 20.3859375,21.534375 20.3859375,21.534375 C20.3859375,21.534375 3.00078125,4.3890625 3.00078125,4.3890625 C3.00078125,4.3890625 4.38984375,3 4.38984375,3 C4.38984375,3 21.7273438,20.0976563 21.7273438,20.0976563 L21.7265625,20.0976563 Z"/>
-</svg></button></div><div>`;
+</svg></button></div><div><button>reset</button>`;
   for(var i in colorSwatches) {
     colorPicker += `<button style='background:${colorSwatches[i][0]};color:${colorSwatches[i][2]}'>${i}</button>`;
   }
